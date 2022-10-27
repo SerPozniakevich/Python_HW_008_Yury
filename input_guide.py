9ced6ee0e0f22310bbb98be630447e1a9b4c574e
Sergey Pozniakevich <ven07@yandex.ru>
2022-10-27 17:52:19 +0700

    add modul input

---
+++ input_guide.py
@@ -0,0 +1,21 @@
+
+
+def input_fam():
+        a = input('Введите фамилию контакта: ')
+        return a
+
+
+def input_name():
+        b = input('Введите имя контакта: ')
+        return b
+
+
+def input_number():
+        c = input('Введите номер телефона контакта: ')
+        return c
+
+
+
+def input_description():
+        d = input('Введите описание: ')
+        return d
